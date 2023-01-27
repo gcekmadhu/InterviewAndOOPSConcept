6919dde3e8018a7a05f66c4abcfc0706d3b75c38
okahate <onkarkahate1210@gmail.com>
2023-01-27 18:11:39 +0530

    Added print statement

--- src/com/oops/concept/GitTest.java
+++ src/com/oops/concept/GitTest.java
@@ -3,5 +3,6 @@ package com.oops.concept;
 public class GitTest {
     public static void main(String args[]){
         System.out.println("Hi Madhuri");
+        System.out.println("Hi Madhuri");
     }
 }
