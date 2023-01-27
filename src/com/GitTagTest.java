e291c84473490c2f8da45f1245a8a366f2ad0112
Madhuri Kulkarni <gcek.madhu@gmail.com>
2023-01-27 23:07:59 +0530

    Git tag Test

---
+++ src/com/GitTagTest.java
@@ -0,0 +1,14 @@
+package com;
+
+//git checkout -b <new feature branch>
+//git add. --->add file
+//git commit -m "<msg>"
+//git tag <v1.0>
+//git push origin v1.0
+//on github publish release
+
+public class GitTagTest {
+    public static void main(String[] args){
+        System.out.println("Git Tag Test");
+    }
+}
